a2b985b6f8b11e5da19323d831402df1adf85589
Karl Benjamin Bughaw <pepe.kotelurow@gmail.com>
2025-06-13 16:31:50 +0000

    corrected sample url:   admin_app/admin_main.py

--- admin_app/admin_main.py
+++ admin_app/admin_main.py
@@ -4,6 +4,7 @@ from admin_app.admin_app import app
 from admin_app.admin_api import AdminApi
 
 def run_flask():
+    # The admin app runs on port 5001
     app.run(host='127.0.0.1', port=5001)
 
 if __name__ == '__main__':
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     api = AdminApi()
     webview.create_window(
         'PUP Shop Admin',
-        'http://12vert.net',
+        'http://127.0.0.1:5001',
         js_api=api,
         width=1024,
         height=768
